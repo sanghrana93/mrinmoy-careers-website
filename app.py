a04d4cf28c2da828a24c643f246d6b6e085f1f55
sanghrana93 <sanghrana93@gmail.com>
2024-12-14 20:53:38 +0000

    add the flask project

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "Hello, Mrinmoy!"
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
+# print("Hello, World!" + " - " + "I am a Python program.")
+# from flask import Flask, render_template, request
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# db = SQLAlchemy(app)
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(200), nullable=False)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
+
+  
+      
